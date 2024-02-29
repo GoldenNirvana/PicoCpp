700b6972c140d8fd1d7ea0188071d3fdfe294398
sfelshtyn <sfelshtyn@mail.ru>
2024-02-29 16:49:51 +0300

    resonance

--- spi/spi_master_slave/spi_master/physical_devices/scanner.cpp
+++ spi/spi_master_slave/spi_master/physical_devices/scanner.cpp
@@ -2683,46 +2683,50 @@ void Scanner::approacphm(std::vector<int32_t> &vector) //uint16_t
 
 void Scanner::start_frqscan()
 {
-  static uint16_t inBuf[5]; // n, start_freq, step, delay
+
   int16_t signalvalue;
   int16_t res_freq = 10000;
   int16_t a = 10000;
   int16_t scan_index = 0;
   int16_t current_freq = 0;
- // int32_t current_channel;
-  for (int j = 0; j < 4; ++j)
+  int16_t freq;
+  int16_t freqstep;
+  int16_t delay;
+  int16_t freqstart;
+  int16_t npoint;
+  // n, start_freq, step, delay
+    npoint=vector[1];
+ freqstart=(uint32_t)vector[2];
+  freqstep=(uint32_t)vector[3];
+     delay=vector[4];
+
+  for (int j = 0; j < 5; ++j)
   {
-    inBuf[j] = vector[1 + j];
-    debugdata.emplace_back(inBuf[j]);
+    debugdata.emplace_back(vector[j]);
   }
- // debugdata.emplace_back(flgVirtual);
- // debugdata.emplace_back(AmplPin);
-//  debugdata.emplace_back(ZPin);
   sendStrData("debug frq scan parameters ",debugdata,100,true);
-  
- // current_channel = inBuf[3] - 1;  
 
   std::vector<int32_t> data;
-
-  while ((scan_index++ < inBuf[0]))
+  freq=freqstart;
+  while ((scan_index++ < npoint))
   {
     if (!flgVirtual)
     {
-      set_Freq(inBuf[1]);
-      sleep_ms(inBuf[3]); //edited 240226
-      data.emplace_back((int32_t)inBuf[1]);
+      set_Freq(freq);
+      sleep_ms(delay); 
+      data.emplace_back((int32_t)freq);
       data.emplace_back((int32_t)getValuesFromAdc()[AmplPin]); //edited 240226
     }
     else
     {
-      current_freq = inBuf[1];
-      sleep_ms(inBuf[3]);
+      current_freq =freq;
+      sleep_ms(delay);
       signalvalue = (int16_t) std::round(a * (pow(M_E, -pow((current_freq - res_freq), 2) / 1000000))); //231126
       data.emplace_back((int32_t)current_freq);
       data.emplace_back((int32_t)signalvalue);
     }
     sleep_ms(10);
-    inBuf[1] += inBuf[2];
+    freq += freqstep;
   }
   sendStrData("code25",data,100,true);
   //current_channel = -1;
