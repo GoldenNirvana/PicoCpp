519f36b0a7d6c4b4d2857e4b0ac6c29fde5024b1
sfelshtyn <sfelshtyn@mail.ru>
2024-04-25 11:01:00 +0300

    error configHW

--- spi/spi_master_slave/spi_master/loop/common_data/device_variables.cpp
+++ spi/spi_master_slave/spi_master/loop/common_data/device_variables.cpp
@@ -1,5 +1,5 @@
 #include "device_variables.hpp"
 
 //Scanner *scanner;
-ConfigHardWare confighardwarev0({1,2,116,7,10,17,PICO_DEFAULT_LED_PIN,23,11,12,12,14,15,26,27});
-ConfigHardWare confighardwarev1({1,2,116,7,10,17,PICO_DEFAULT_LED_PIN,23,11,12,12,14,15,26,27});
+ConfigHardWare confighardwarev0({1,2,1,16,7,10,17,PICO_DEFAULT_LED_PIN,23,11,12,12,14,15,26,27});
+ConfigHardWare confighardwarev1({1,2,1,16,7,10,17,PICO_DEFAULT_LED_PIN,23,11,12,12,14,15,26,27});
