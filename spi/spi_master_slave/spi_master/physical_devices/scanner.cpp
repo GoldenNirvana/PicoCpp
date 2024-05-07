1ee84609b266e8e1a87e36cba89a702357e5ab40
mf <mf@mf.ru>
2024-05-07 18:05:21 +0300

    error normalize

--- spi/spi_master_slave/spi_master/physical_devices/scanner.cpp
+++ spi/spi_master_slave/spi_master/physical_devices/scanner.cpp
@@ -106,7 +106,7 @@ void Scanner::readDATALin()
    } 
 //  sendStrData("debug liny ",data_LinY,400,false);
 }
-  
+  /*
 void Scanner::readADC()
 {
   if (!flgVirtual)
@@ -141,7 +141,42 @@ void Scanner::readADC()
         sendStrData("code"+std::to_string(ADC_READCmd),debugdata,100,true);     
   }
 }
-
+*/
+void Scanner::readADC()
+{
+  if (!flgVirtual)
+  {
+     hardware->getValuesFromAdc();
+  // auto ptr = hardware->getValuesFromAdc();
+  //logger(ptr, 8);
+   ZValue = (int16_t)spiBuf[ZPin];
+      switch (vector[1]) //прибор
+   {
+        case SFM: //SFM=0
+                {
+                 SignalValue = (int16_t) spiBuf[AmplPin];
+                 break;  
+                } 
+        case STM://STM=1
+     case SICMDC://SICMDC=3  
+                {
+                 SignalValue = (int16_t) spiBuf[IPin];
+                 break;  
+                } 
+   }         
+        debugdata.emplace_back(ZValue);
+        debugdata.emplace_back(SignalValue);
+        debugdata.emplace_back(vector[1]);
+        sendStrData("code"+std::to_string(ADC_READCmd),debugdata,100,true);
+  } 
+  else
+  {
+        debugdata.emplace_back(ZValue);
+        debugdata.emplace_back(SignalValue);
+        debugdata.emplace_back(vector[1]);
+        sendStrData("code"+std::to_string(ADC_READCmd),debugdata,100,true);     
+  }
+}
 bool Scanner::getHoppingFlg() //получить флаг установлен ли флаг сканирования прыжками
 {
   return (bool)conf_.flgHoping;
