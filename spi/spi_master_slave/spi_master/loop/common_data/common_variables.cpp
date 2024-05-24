db834ac0e08a8154e09407d222f86cc95845cccd
sfelshtyn <sfelshtyn@mail.ru>
2024-05-24 18:05:06 +0300

    rename branch

--- spi/spi_master_slave/spi_master/loop/common_data/common_variables.cpp
+++ spi/spi_master_slave/spi_master/loop/common_data/common_variables.cpp
@@ -1,7 +1,7 @@
 #include "common_variables.hpp"
 
 //////////////////////////////////////////////
-std::string  SOFTVERSION="24.05.07.01 HW2 pointer";
+std::string  SOFTVERSION="24.05.07.01 HWA pointer";
 std::string  HARDWAREVERSION="0.1";
 int8_t       HARDWAREVERSION_I=0;
 //int8_t       HARDWAREVERSION_I=-1;
