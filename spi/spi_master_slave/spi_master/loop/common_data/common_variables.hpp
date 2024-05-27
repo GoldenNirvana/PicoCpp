f39a37ec887af9e08465a756f1a95dc54c15c932
sfelshtyn <sfelshtyn@mail.ru>
2024-05-27 21:01:42 +0300

    atomic

--- spi/spi_master_slave/spi_master/loop/common_data/common_variables.hpp
+++ spi/spi_master_slave/spi_master/loop/common_data/common_variables.hpp
@@ -60,7 +60,7 @@
 
 extern std::string  SOFTVERSION;
 extern std::string  HARDWAREVERSION;
-extern int16_t  ALGCODE;
+std::atomic<int16_t>  ALGCODE;
 extern uint32_t DEBUG_LEVEL;
 extern Spi      spi;
 extern LinearDriver linearDriver;
@@ -89,18 +89,25 @@ extern bool ADC_GET_VALUE;     //AD7606
 
 extern bool LID;
 extern bool SET_IO_VALUE;
-extern bool CONFIG_UPDATE;
+;
 extern bool Z_STATE;
 // add MF
+extern std::atomic<bool> CONFIG_UPDATE;
+extern std::atomic<bool> STOP;
+extern std::atomic<bool> TheadDone;   //need dor synchronization with PC 
+extern std::atomic<bool> DrawDone;
+extern std::atomic<bool> ADC_IS_READY_TO_READ;
+//extern bool STOP;
+//extern bool ADC_IS_READY_TO_READ;
+//extern bool CONFIG_UPDATE
+//extern bool TheadDone;   //need dor synchronization with PC 
+//extern bool DrawDone;
 extern bool LOOP_FREEZE_UNFREEZE;
-extern bool STOP;
 extern bool PID_TURN_ON;
 extern bool SCANNER_RETRACT;
 extern bool SCANNER_PROTRACT;
-extern bool ADC_IS_READY_TO_READ;
+
 // service flags
-extern bool TheadDone;   //need dor synchronization with PC 
-extern bool DrawDone;
 extern bool flgParamsUpdated;
 extern bool flgDebug;
 extern bool flgVirtual;  // Virtual device for debuging and simulation
