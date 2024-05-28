1fabe23cac9eaae426bdf737c5bd220a8fc92929
sfelshtyn <sfelshtyn@mail.ru>
2024-05-28 11:34:20 +0300

    set atomic var

--- spi/spi_master_slave/spi_master/loop/common_data/common_variables.cpp
+++ spi/spi_master_slave/spi_master/loop/common_data/common_variables.cpp
@@ -12,10 +12,10 @@ std::vector<int32_t> vupdateparams;
 int32_t vectorSize;
 uint16_t spiBuf[8];
 std::atomic<int16_t>      ALGCODE;
-std::atomic<bool> CONFIG_UPDATE;
-std::atomic<bool> STOP;
-std::atomic<bool> TheadDone;   //need dor synchronization with PC 
-std::atomic<bool> DrawDone;
+std::atomic<bool> CONFIG_UPDATE=false;
+std::atomic<bool> STOP=true;
+std::atomic<bool> TheadDone=true;   //need dor synchronization with PC 
+std::atomic<bool> DrawDone=true;
 std::atomic<bool> ADC_IS_READY_TO_READ = true;
 bool AD9833_SENDER = false;
 bool AD8400_SENDER = false;
